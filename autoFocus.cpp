247c5e4ff2377b9558b7bae2b52f11cc9f2a2553
Stanislav Egorov <segorov@mirantis.com>
2012-10-17 16:16:48 +0400

    added socket serv&client

--- autoFocus.cpp
+++ autoFocus.cpp
@@ -1,40 +1,66 @@
-//
-// пример работы оператора Лапласа - cvLaplace()
-//
-// robocraft.ru
-//
 
 #include <cv.h>
 #include <highgui.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 IplImage* image = 0;
 IplImage* dst = 0;
 IplImage* dst2 = 0;
 
-int main(int argc, char* argv[])
-{
-	// имя картинки задаётся первым параметром
-	char* filename = "image0.jpg";
+char message[] = "Hello there!\n";
+char buf[sizeof(message)];
 
-	if (argv[1]) filename = argv[1];
-	// получаем картинку
+int send_cmd(char *cmd, int port)
+{
+    int sock;
+    struct sockaddr_in addr;
+
+    if (! cmd) return -1;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect");
+        exit(2);
+    }
+    printf("sending %s\n", cmd);
+    send(sock, cmd, sizeof(cmd), 0);
+    recv(sock, buf, sizeof(cmd), 0);
+
+    printf("received %s\n", buf);
+    close(sock);
+
+    return 0;
+}
+
+short getFocus(char *filename)
+{
+	if (! filename) return -1;
 	image = cvLoadImage(filename, 1);
-	// создаём картинки
 	dst = cvCreateImage( cvGetSize(image), IPL_DEPTH_16S, image->nChannels);
 	dst2 = cvCreateImage( cvGetSize(image), image->depth, image->nChannels);
 
 	printf("[i] image: %s\n", filename);
 	assert( image != 0 );
 
-	// окно для отображения картинки
 	cvNamedWindow("original", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("cvLaplace", CV_WINDOW_AUTOSIZE);
 
-	int aperture = argc == 3 ? atoi(argv[2]) : 3;
+	int aperture = 3;
 
-	// применяем оператор Лапласа
 	cvLaplace(image, dst, aperture);
 	short maxLap = -32767;
 	short* imgData = (short*)dst->imageData;
@@ -44,20 +70,33 @@ int main(int argc, char* argv[])
 	}
 	printf("maxLap=%d\n", maxLap);
 
-	// преобразуем изображение к 8-битному
-	cvConvertScale(dst, dst2);
+	//cvConvertScale(dst, dst2);
 
-	// показываем картинку
-	cvShowImage("original", image);
-	cvShowImage("cvLaplace", dst2);
+	//cvShowImage("original", image);
+	//cvShowImage("cvLaplace", dst2);
 
-	cvWaitKey(0);
+	//cvWaitKey(0);
 
-	// освобождаем ресурсы
 	cvReleaseImage(&image);
 	cvReleaseImage(&dst);
 	cvReleaseImage(&dst2);
-	// удаляем окна
 	cvDestroyAllWindows();
+	return maxLap;
+}
+
+int main(int argc, char* argv[])
+{
+	short tmpLap, curLap;
+	int i;
+
+	if (argc <= 0) return -1;
+	for (i=1; i<argc; i++)
+	{
+		tmpLap = getFocus(argv[i]);
+		if (tmpLap < 900) send_cmd("focusIn", 3425);
+		if (tmpLap > curLap) curLap = tmpLap;
+	}
+	printf("actually maximum Lap=%d\n", curLap);
+
 	return 0;
 }
